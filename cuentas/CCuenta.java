73714a2825095223c59d491383daf364e17bebfd
daniel <danielerce@hotmail.com>
2024-04-04 01:31:23 +0200

    añadido javadoc

--- cuentas/CCuenta.java
+++ cuentas/CCuenta.java
@@ -1,5 +1,8 @@
 package cuentas;
 
+/**
+ * clase cuenta almacena los atributos nombre, cuenta, saldo y tipo de interes
+ */
 public class CCuenta {
 
     private String nombre;
@@ -10,40 +13,88 @@ public class CCuenta {
     public CCuenta() {
     }
 
+    /**
+     * constructor de clase cuenta
+     *
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         nombre = nom;
         cuenta = cue;
         saldo = sal;
     }
 
+    /**
+     * getter del atributo nombre
+     *
+     * @return nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * setter del atributo nombre
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * getter del atributo cuenta
+     *
+     * @return cuenta
+     */
     public String getCuenta() {
         return cuenta;
     }
 
+    /**
+     * setter del atributo cuenta
+     *
+     * @param cuenta
+     */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
+    /**
+     * getter del atributo saldo
+     *
+     * @return saldo
+     */
     public double getSaldo() {
         return saldo;
     }
 
+    /**
+     * setter del atributo saldo
+     *
+     * @param saldo
+     */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
+    /**
+     * getter del atributo tipoInteres
+     *
+     * @return tipoInteres
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
+    /**
+     * setter del atributo tipoInteres
+     *
+     * @param tipoInterés
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
